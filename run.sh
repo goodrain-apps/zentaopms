e175664ae00f8650007f8debeeb8f9d390d822ed
zhouyq <zyq916@gmail.com>
2016-03-28 18:44:57 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,18 +3,27 @@
 Dirs="www/data module config tmp"
 PermanentDir="/data"
 AppDir="/app"
+UserCfg="${PermanentDir}/config/my.php"
+InstallFile="${AppDir}/www/install.php"
+UpgradeFile="${AppDir}/www/upgrade.php"
 
 # 在持久化存储中创建需要的目录
 for d in $Dirs
 do
-  if [ ! -d /${PermanentDir}/${d} ] ;then
-    [ -d /${AppDir}/${d} ] && mv /${AppDir}/${d} /${PermanentDir}/${d} || mkdir -pv /${PermanentDir}/${d}
+  if [ ! -d ${PermanentDir}/${d} ] ;then
+    [ -d ${AppDir}/${d} ] && mv ${AppDir}/${d} ${PermanentDir}/${d} || mkdir -pv ${PermanentDir}/${d}
   else
-    rm -rf /${AppDir}/${d}
+    rm -rf ${AppDir}/${d}
   fi
 
-  ln -s /${PermanentDir}/${d} /${AppDir}/${d}
+  ln -s ${PermanentDir}/${d} ${AppDir}/${d}
 done
+
+# 如果存在my.php 清理install.php和upgrade.php 文件
+if [ -f $UserCfg ];then
+  [ -f $InstallFile ] && rm -f $InstallFile
+  [ -f $UpgradeFile ] && rm -f $UpgradeFile
+fi
 
 
 # 启动web server
